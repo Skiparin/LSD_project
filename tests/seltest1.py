e8d9738989e0f207ca28aa6808ae732742260d25
Laura <laurahartig@Lauras-MacBook.local>
2018-09-24 13:22:04 +0200

    setting up Selenium incl. test

---
+++ tests/seltest1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import unittest
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+
+class TestUbuntuHomepage(unittest.TestCase):
+
+    def setUp(self):
+        options = Options()
+        options.add_argument("--headless")
+        self.browser = webdriver.Firefox(firefox_options=options, executable_path="/Users/laurahartig/Webdriver/geckodriver")
+
+    def testTitle(self):
+        self.browser.get('http://www.ubuntu.com/')
+        self.assertIn('Ubuntu', self.browser.title)
+
+    def tearDown(self):
+        self.browser.quit()
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)

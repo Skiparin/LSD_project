665e0da0ecdbbf7ce853fb0c85f8f5f9d19e5257
Skiparin <og1806x9@hotmail.com>
2018-09-24 19:34:21 +0000

    needs to host on 0.0.0.0

--- api/endpoint.py
+++ api/endpoint.py
@@ -34,4 +34,4 @@ def sqlalchemy_json(dictionary):
 	return json.dumps([dict(r) for r in dictionary],default=str)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)

0c4312a15b6bd1f642890f377ba795597b6c749d
Thesoap <tvh_1994@hotmail.dk>
2018-09-27 12:38:09 +0200

    ulækker comment SQL statement

--- api/endpoint.py
+++ api/endpoint.py
@@ -47,7 +47,52 @@ def login():
 @app.route('/comments')
 def comments():
     post_id = request.args.get('post_id')
-    sql_statement = f"select * from comments where post_id = '{post_id}'"
+    sql_statement = f"""
+    WITH RECURSIVE cte (id, content, username, path, parent_id, depth, karma)  AS (
+    SELECT  
+        comments.id,
+        content,
+        users.username,
+        array[-(karma.upvotes - karma.downvotes),comments.id] AS path,
+        parent_id,
+        1 AS depth,
+        karma.upvotes
+    FROM 
+        ( 
+            comments INNER JOIN karma
+            ON comments.karma_id = karma.id
+            INNER JOIN users
+            ON comments.user_id = users.id
+        )
+    WHERE parent_id IS NULL
+    UNION ALL
+    SELECT
+        comments.id,
+        comments.content,
+        users.username,
+        cte.path || -(karma.upvotes - karma.downvotes) || comments.id,
+        comments.parent_id,
+        cte.depth + 1 AS depth,
+        karma.upvotes
+    FROM    
+        ( 
+            comments INNER JOIN karma
+            ON comments.karma_id = karma.id
+            INNER JOIN users
+            ON comments.user_id = users.id
+        )
+    JOIN cte ON comments.parent_id = cte.id
+    )
+    SELECT 
+        id, 
+        content,
+        username,
+        path,
+        depth,
+        karma
+    FROM cte
+    ORDER BY path;
+    """
     con = db_connect(engine) 
     sqlalchemy_object = con.execute(sql_statement)
     json_list = sqlalchemy_json(sqlalchemy_object)

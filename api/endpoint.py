f6af4ff85d69dbb890f67fc834c40876a655cdaf
Michael <2750daugbjerg@gmail.com>
2018-10-01 10:11:50 +0200

    working on posts in html

--- api/endpoint.py
+++ api/endpoint.py
@@ -62,6 +62,7 @@ def sqlalchemy_json(dictionary):
 
 @app.route('/sortedposts')
 def sort_posts():
+    post_list = {}
     jobject = posts()
     post_list = json.loads(jobject)
     return render_template('frontpage.html', post_list=post_list)

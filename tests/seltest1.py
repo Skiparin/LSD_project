0ba6cb5cf52ce27cf9b42518832f1fa0a51e3b61
Michael <2750daugbjerg@gmail.com>
2018-10-14 22:26:54 +0200

    working with selenium tests

--- tests/seltest1.py
+++ tests/seltest1.py
@@ -13,12 +13,12 @@ class TestUbuntuHomepage(unittest.TestCase):
 
     #Setting up the driver in headless mode
     @classmethod
-    def set_up(self):
+    def set_up(cls):
         options = Options()
         options.add_argument("--headless")
-        
-        self.driver = webdriver.Firefox(options=options, executable_path="/usr/bin/geckodriver")
-        self.driver.get("http://159.65.116.24:5004/sortedposts")
+
+        cls.driver = webdriver.Firefox(options=options, executable_path="/usr/bin/geckodriver")
+        cls.driver.get("http://159.65.116.24:5004/sortedposts")
 
     #This returns us to the start page every time a test finished.
     def tear_down(self):

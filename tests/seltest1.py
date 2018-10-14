626935b1bc761194feda4145b87851304f6faaed
Michael <2750daugbjerg@gmail.com>
2018-10-14 22:50:35 +0200

    working with selenium tests

--- tests/seltest1.py
+++ tests/seltest1.py
@@ -32,7 +32,7 @@ class TestUbuntuHomepage(unittest.TestCase):
     def test_get_posts(self):
         print("test get posts")
         self.result = self.driver.find_element_by_xpath("//tr[@class='athing']/td/span").text
-        self.expected = 1
+        self.expected = '1'
         self.assertEqual(self.result, self.expected)
 
 if __name__ == '__main__':

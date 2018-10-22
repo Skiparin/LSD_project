3945acbabdd5bcf3e3c58600cb58f768bcbfcb3e
Laura <laurahartig@Lauras-MacBook.local>
2018-10-22 13:02:54 +0200

    first unittest

--- tests/unit_testing.py
+++ tests/unit_testing.py
@@ -4,13 +4,13 @@ from unittest.mock import patch
 parentPath = os.path.abspath("../api")
 if parentPath not in sys.path:
     sys.path.insert(0,parentPath)
-from endpoint import sort_posts
+from endpoint import posts
 
 class unit_testing(unittest.TestCase):
 
     def test_content_from_post(self):
         expected = "Henrietta er swag"
-        post_dict = sort_posts()
+        post_dict = posts()
         self.assertTrue(expected in post_dict)
 
 if __name__ == '__main__':

203b7e70053371ab318f9684673ad795beaeeb89
Laura <laurahartig@Lauras-MacBook.local>
2018-10-22 14:39:19 +0200

    more unittests, not working (needs merge with dev)

--- tests/unit_testing.py
+++ tests/unit_testing.py
@@ -1,8 +1,12 @@
 import unittest
 import json
 import os, sys
-from endpoint import *
-from sql_statements import *
+from unittest.mock import patch
+parentPath = os.path.abspath("../api")
+if parentPath not in sys.path:
+    sys.path.insert(0,parentPath)
+from endpoint import posts #, create_post
+from sql_statements import *#find_post_with_hanesst_id
 
 class unit_testing(unittest.TestCase):
 
@@ -21,10 +25,25 @@ class unit_testing(unittest.TestCase):
         "post_title":"Største noob i verden",
         "hanesst_id":99999,
         "post_url":"https://www.facebook.com/orvurguttesen"}'''
-        post_json = json.load(expected)
+        post_json = json.loads(expected)
         create_post(post_json)
         post_id = find_post_with_hanesst_id(99999)
         self.assertTrue(post_id)
+
+     def test_create_comment(self):
+        print("Testing test_create_comment")
+        expected = '''{
+        "post_type":"comment",
+        "username":"orvur2",
+        "pwd_hash":"1234",
+        "post_title":"Candy Crush Friends sucks",
+        "hanesst_id":100000,
+        "post_url":"https://www.facebook.com/orvurguttesen"}'''
+        comment_json = json.loads(expected)
+        create_comment(comment_json)
+        comment_id = find_comment_with_hanesst_id(100000)
+        self.assertTrue(comment_id)
+
 
 if __name__ == '__main__':
     unittest.main()

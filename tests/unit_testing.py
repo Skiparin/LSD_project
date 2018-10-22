7ca616e2c58d59fd24b12ba8fd7a7fb1f327f7d2
Laura <laurahartig@Lauras-MacBook.local>
2018-10-22 11:15:04 +0200

    first unittest

--- tests/unit_testing.py
+++ tests/unit_testing.py
@@ -1,11 +1,11 @@
 import unittest
-import api.endpoint as endpoint
+from api.endpoint import sort_posts
 
 class unit_testing(unittest.TestCase):
 
     def test_content_from_post(self):
         expected = "Henrietta er swag"
-        post_dict = endpoint.sort_posts()
+        post_dict = sort_posts()
         self.assertTrue(expected in post_dict)
 
 if __name__ == '__main__':

a5aa2aa22ffc232a3c939502909e702c8774a41c
Laura <laurahartig@Lauras-MacBook.local>
2018-10-22 14:13:18 +0200

    first unittest

--- tests/unit_testing.py
+++ tests/unit_testing.py
@@ -1,18 +1,30 @@
 import unittest
+import json
 import os, sys
-from unittest.mock import patch
-parentPath = os.path.abspath("../api")
-if parentPath not in sys.path:
-    sys.path.insert(0,parentPath)
-from endpoint import posts
+from endpoint import *
+from sql_statements import *
 
 class unit_testing(unittest.TestCase):
 
     def test_content_from_post(self):
+        print("Testing test_content_from_post")
         expected = "Henrietta er swag"
         post_dict = posts()
-        print(post_dict)
         self.assertEquals(expected, post_dict[0]["content"])
+
+    def test_create_post(self):
+        print("Testing test_create_post")
+        expected = '''{
+        "post_type":"story",
+        "username":"orvur",
+        "pwd_hash":"1234",
+        "post_title":"Største noob i verden",
+        "hanesst_id":99999,
+        "post_url":"https://www.facebook.com/orvurguttesen"}'''
+        post_json = json.load(expected)
+        create_post(post_json)
+        post_id = find_post_with_hanesst_id(99999)
+        self.assertTrue(post_id)
 
 if __name__ == '__main__':
     unittest.main()

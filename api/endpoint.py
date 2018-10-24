ca830dd8fb11d1274882f51fc3c7d95f9d8d41f7
Nicolai Mikkelsen <n_r_mikkelsen@hotmail.com>
2018-10-24 11:43:28 +0200

    comment_api fixed comment metod

--- api/endpoint.py
+++ api/endpoint.py
@@ -88,7 +88,7 @@ def comment(json):
         hanesst_id = json['hanesst_id']
         post_id = ss.find_post_with_hanesst_id(post_parent)
         if post_id == None:
-            comment_dict = ss.find_comment_with_hanesst_id(parent_id)
+            comment_dict = ss.find_comment_with_hanesst_id(post_parent)
             post_id = comment_dict['post_id']
             parent_id = comment_dict['id']
             insert_comment_on_comment(post_id, content, parent_id, user_id, hanesst_id)

c2408a145e741b875ed248242aafb29b25b39f0f
Michael <2750daugbjerg@gmail.com>
2018-11-05 18:49:42 +0100

    working on prometheus

--- api/endpoint.py
+++ api/endpoint.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask import request
 from flask import render_template
-from prometheus_flask_exporter import PrometheusMetrics
+from prometheus_client import Counter
 import sql_statements as sql_statements
 import json
 import requests
@@ -9,9 +9,6 @@ import logging
 
 
 app = Flask(__name__)
-metrics = PrometheusMetrics(app)
-# static information as metric
-metrics.info('app_info', 'Application info', version='1.0.3')
 
 @app.route('/latest')
 def latest():

825715b2a2577114e440f9728fa65ac23ba8c2c3
Michael <2750daugbjerg@gmail.com>
2018-11-12 10:16:08 +0100

    prometheus

--- api/endpoint.py
+++ api/endpoint.py
@@ -189,4 +189,4 @@ def sort_posts():
 if __name__ == '__main__':
     logging.basicConfig(format='%(asctime)s:%(levelname)s:%(message)s',filename='logfile.log',level=logging.DEBUG)
     monitor(app, port=5102)
-    app.run(debug=True,host="0.0.0.0", port=5004)
+    app.run(host="0.0.0.0", port=5004)
